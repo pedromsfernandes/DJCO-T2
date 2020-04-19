63dee6f39e354f2efd37f4502ec2dd0b5deacc3c
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-04-19 14:54:54 +0000

    Add basic camera movement

---
+++ Light The Way/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float rotX = 0.0f;
+    float rotY = 0.0f;
+
+    public float speed = 120.0f;
+    public float inputSensitivity = 150.0f;
+    public float clampAngle = 80.0f;
+    public GameObject follow;
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        rotX -= mouseY * inputSensitivity * Time.deltaTime;
+        rotY += mouseX * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+        Transform target = follow.transform;
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
