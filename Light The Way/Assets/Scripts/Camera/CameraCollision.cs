2c8fce76696d2f9d04bc50428d16bbca3de9009f
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-04-19 15:13:40 +0000

    Add collision detection to avoid clipping

---
+++ Light The Way/Assets/Scripts/Camera/CameraCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    Vector3 direction;
+    float distance;
+
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+
+    void Start()
+    {
+        direction = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;        
+    }
+
+    void Update()
+    {
+        Vector3 target = transform.parent.TransformPoint(direction * maxDistance);        
+        RaycastHit hit;
+
+        if(Physics.Linecast(transform.parent.position, target, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, direction * distance, Time.deltaTime * smooth);
+    }
+}
