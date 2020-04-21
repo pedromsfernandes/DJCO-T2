6e6f4e81079c9667fab34e0c9059df1e7f240cde
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2020-04-21 20:17:47 +0100

    Add sun rotation testing scene

---
+++ Light The Way/Assets/Scripts/Lighting/SunController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lighting
+{
+    public class SunController : MonoBehaviour
+    {
+        public float rotationSpeed;
+
+        private Transform _transform;
+        
+        private float _finalRotation;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _transform.rotation = Quaternion.RotateTowards(_transform.rotation, 
+                Quaternion.Euler(_finalRotation, 0, 0), 
+                Time.deltaTime * rotationSpeed);
+        }
+
+        public void SetSunHour(float hour)
+        {
+            _finalRotation = hour * 270 / 24 - 45;
+        }
+    }
+}
---
+++ Light The Way/Assets/Scripts/temp/SliderButton.cs
@@ -0,0 +1,34 @@
+﻿using Lighting;
+using TMPro;
+using UnityEngine;
+
+namespace temp
+{
+    public class SliderButton : MonoBehaviour
+    {
+        private float _value;
+        private TextMeshProUGUI _textMeshPro;
+        private SunController _sunController;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
+            _sunController = GameObject.FindWithTag("Sun").GetComponent<SunController>();
+            
+            UpdateText(9);
+            UpdateSun();
+        }
+
+        public void UpdateText(float value)
+        {
+            _value = value;
+            _textMeshPro.SetText($"Update Sun: {value}");
+        }
+
+        public void UpdateSun()
+        {
+            _sunController.SetSunHour(_value);
+        }
+    }
+}
