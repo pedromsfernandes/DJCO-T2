56e84444779fd9f7c77de7c99528a6602ce26048
Dinis Moreira <dinismoreira1997@gmail.com>
2020-04-23 01:06:26 +0100

    Added Basic Movement

---
+++ Light The Way/Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    private float currentSpeed = 0f;
+    private float speedSmoothVelocity = 0f;
+    private float speedSmoothTime = 0.1f;
+    private float rotationSpeed = 0.1f;
+    private float gravity = 3f;
+
+    private Transform mainCameraTransform;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 forward = mainCameraTransform.forward;
+        Vector3 right = mainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
+
+        //Gravity
+        /*Vector3 gravityVector = Vector3.zero;
+
+        if (!controller.isGrounded)
+        {
+            gravityVector.y -= gravity;
+        }
+
+        controller.Move(gravityVector * Time.deltaTime);*/
+
+        if(desiredMoveDirection != Vector3.zero)
+        {
+            //Rotate player in the desired direction
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+        }
+
+        float targetSpeed = movementSpeed * movementInput.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        controller.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
+    }
+}
