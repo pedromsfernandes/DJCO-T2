751d82344b8553e2d31d3ab7b59f1b3ed214d342
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2020-04-28 15:44:02 +0100

    Add scrollwheel input to the sun

--- Light The Way/Assets/Scripts/Lighting/SunController.cs
+++ Light The Way/Assets/Scripts/Lighting/SunController.cs
@@ -8,25 +8,21 @@ namespace Lighting
         public float rotationSpeed;
 
         private Transform _transform;
-        
-        private float _finalRotation;
 
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             _transform = transform;
         }
 
         private void Update()
         {
-            _transform.rotation = Quaternion.RotateTowards(_transform.rotation, 
-                Quaternion.Euler(_finalRotation, 0, 0), 
-                Time.deltaTime * rotationSpeed);
-        }
-
-        public void SetSunHour(float hour)
-        {
-            _finalRotation = hour * 270 / 24 - 45;
+            float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
+            
+            if (Math.Abs(scrollAxis) > 0.01)
+            {
+                _transform.Rotate(Math.Sign(scrollAxis) * Time.deltaTime * rotationSpeed, 0, 0);
+            }
         }
     }
 }
