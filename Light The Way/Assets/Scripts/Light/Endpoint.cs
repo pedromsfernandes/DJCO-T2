0f79fdb57c8d6cbcc46faa3b4a76e033c57b7a27
Dinis Moreira <dinismoreira1997@gmail.com>
2020-06-02 20:49:22 +0100

    Added Hit endpoint sound logic

--- Light The Way/Assets/Scripts/Light/Endpoint.cs
+++ Light The Way/Assets/Scripts/Light/Endpoint.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Photon.Pun;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -18,6 +19,13 @@ namespace Light
 
         private LightColor currentColor = LightColor.Of(LightType.None);
 
+        private LightColor previousColor = LightColor.Of(LightType.None);
+
+        //sound
+        [FMODUnity.EventRef]
+        private string selectedHitEndpointSound = "event:/FX/Crystal/CrystalHit";
+        FMOD.Studio.EventInstance hitEndpointSoundEvent;
+
         private void Start()
         {
             _endpointBeam = transform.GetChild(0).GetComponent<LightBeam>();
@@ -28,12 +36,16 @@ namespace Light
 
             _endpointBeam.gameObject.SetActive(true);
 
+            //sound
+            hitEndpointSoundEvent = FMODUnity.RuntimeManager.CreateInstance(selectedHitEndpointSound);
         }
 
         protected override void OnBeamSense(LightBeam beam, RaycastHit hit, Vector3 reflectedDirection)
         {
             currentColor.AddColor(beam.LightColor);
         }
+
+        
 
         void LateUpdate()
         {
@@ -65,7 +77,26 @@ namespace Light
             LightBeam.UpdateLightBeam(_endpointBeam.gameObject, beamColor,
                 transform.position, transform.up);
 
+
+            if (!currentColor.ToString().Equals(previousColor.ToString()))
+            {
+                previousColor = currentColor;
+                GetComponent<PhotonView>().RPC("OnBeamSenseSoundSelf", RpcTarget.All, this.name);
+                Debug.Log("Play Hit Sound " + this.name);
+            }
+
             currentColor = LightColor.Of(LightType.None);
+        }
+
+        [PunRPC]
+        void OnBeamSenseSoundSelf(string originalObjectName)
+        {
+            GameObject originalObject = GameObject.Find(originalObjectName);
+            Transform originalTransform = originalObject.GetComponent<Transform>();
+            Rigidbody originalRigidbody = originalObject.GetComponentInChildren<Rigidbody>();
+
+            FMODUnity.RuntimeManager.AttachInstanceToGameObject(hitEndpointSoundEvent, originalTransform, originalRigidbody);
+            hitEndpointSoundEvent.start();
         }
 
         public void Deactivate()
