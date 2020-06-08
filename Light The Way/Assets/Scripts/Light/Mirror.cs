f5fbb2ae6a117f5c42a04d8746e0b3a1d30c0d94
Dinis Moreira <dinismoreira1997@gmail.com>
2020-06-08 11:17:16 +0100

    Added mirror reflection sound

--- Light The Way/Assets/Scripts/Light/Mirror.cs
+++ Light The Way/Assets/Scripts/Light/Mirror.cs
@@ -4,9 +4,43 @@ namespace Light
 {
     public class Mirror : BeamEmitter
     {
+        int numHits = 0;
+
+        //sound
+        [FMODUnity.EventRef]
+        public string selectedMirrorReflectSound = "event:/FX/Crystal/Mirror/MirrorReflect";
+        FMOD.Studio.EventInstance mirrorReflectSoundEvent;
+
+        void Start()
+        {
+            //sound
+            mirrorReflectSoundEvent = FMODUnity.RuntimeManager.CreateInstance(selectedMirrorReflectSound);
+        }
+
+        private void Update()
+        {
+            FMOD.Studio.PLAYBACK_STATE fmodPBState;
+            mirrorReflectSoundEvent.getPlaybackState(out fmodPBState);
+                                 
+            if(numHits > 0 && fmodPBState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                Rigidbody originalRigidbody = this.GetComponentInChildren<Rigidbody>();
+                FMODUnity.RuntimeManager.AttachInstanceToGameObject(mirrorReflectSoundEvent, this.transform, originalRigidbody);
+                mirrorReflectSoundEvent.start();
+            }
+            else if(numHits == 0 && fmodPBState == FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                mirrorReflectSoundEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            }
+
+            numHits = 0;
+        }
+
         protected override void OnBeamSense(LightBeam beam, RaycastHit hit, Vector3 direction)
         {
             EmitBeam(beam.LightColor, hit.point, Vector3.Reflect(direction, hit.normal));
+
+            numHits++;
         }
     }
 }
